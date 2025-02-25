3a8d23d4ae136f799942f732acf8701a638f35c4
barisariburnu <barisariburnu@gmail.com>
2025-02-25 19:43:07 +0300

    Add task routes to the main application

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,7 @@ import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 import workspaceRoutes from "./routes/workspace.route";
 import memberRoutes from "./routes/member.route";
 import projectRoutes from "./routes/project.route";
+import taskRoutes from "./routes/task.routes";
 const app = express();
 const BASE_PATH = config.BASE_PATH;
 
@@ -56,6 +57,7 @@ app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
 app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
 app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
+app.use(`${BASE_PATH}/task`, isAuthenticated, taskRoutes);
 
 app.use(errorHandler);
 
